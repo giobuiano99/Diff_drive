30507e87cf9ca7b75b4a71b7a3570eddbf5dabbd
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 <feesposito00@gmail.com>
2023-05-22 14:45:08 +0200

    Simulation navigation finalized (hopefully)

--- src/goal_publisher.cpp
+++ src/goal_publisher.cpp
@@ -49,91 +49,50 @@ void goal_gen::loop() {
 	double roll, pitch, yaw;
 	tf2::Quaternion quaternion;
 
+	bool sent = false;
 
-	while (ros::ok()) {
-		
+
+	while (!sent && ros::ok()) {
 		try{
-            //_listener.waitForTransform("aruco_marker_frame","camera_frame",ros::Time(0),ros::Duration(3.0));
-			//_listener.lookupTransform("aruco_marker_frame","camera_frame",ros::Time(0), _check_trans);
+			_listener.waitForTransform("camera_frame", "aruco_marker_frame", ros::Time(0), ros::Duration(60.0));
+			_trans.setOrigin( tf::Vector3(0, 0, _disp) );
+			tf::Quaternion q;
+			q.setRPY(0,1.57,-1.57);
+			_trans.setRotation(q);
+			_trans_br.sendTransform(tf::StampedTransform(_trans, ros::Time::now(), "aruco_marker_frame", "goal_frame"));
+			_listener_goal.waitForTransform("map", "goal_frame", ros::Time(0), ros::Duration(60.0));
+			_listener_goal.lookupTransform("map","goal_frame",ros::Time(0), _transform);
 
-		   // compare tfs to check if the marker is in the field of view
-			if (first_check) {
-				first_check = false;
-			} else {
-				if (_check_trans == _prev_trans) {
-					Tf_equal = true;
-				} else {
-					Tf_equal = false;
-				}
-			}
-
-			if (!Tf_equal) {
-				_trans.setOrigin( tf::Vector3(0, 0, _disp) );
-				tf::Quaternion q;
-				q.setRPY(0,1.57,-1.57);
-				_trans.setRotation(q);
-				//_trans_br.sendTransform(tf::StampedTransform(_trans, ros::Time::now(), "aruco_marker_frame", "goal_frame"));
-				//_listener_goal.lookupTransform("map","goal_frame",ros::Time(0), _transform);
-
-				goal.target_pose.header.frame_id="map";
-				goal.target_pose.header.stamp=ros::Time::now();
-				
-
-				//goal.target_pose.pose.position.x =  _transform.getOrigin().x();
-				//goal.target_pose.pose.position.y =  _transform.getOrigin().y();
-				goal.target_pose.pose.position.x = 1.2;
-				goal.target_pose.pose.position.y = 1.2;
-
-				//tf::Matrix3x3 m( _transform.getRotation() ); // quaternion to RPY
-				//m.getRPY(roll, pitch, yaw);
-				//quaternion.setRPY(0,0,yaw-3.14);  // RPY to quaternion 
-				
-				//DEBUG
-				quaternion.setRPY(0,0,0.554); 
-				goal.target_pose.pose.orientation.x = quaternion.x() ; 
-				goal.target_pose.pose.orientation.y = quaternion.y() ; 
-				goal.target_pose.pose.orientation.z = quaternion.z() ; 
-				goal.target_pose.pose.orientation.w = quaternion.w() ; 
-
-
-				/*nav_msg.header.frame_id = "goal_frame";
-				
-				nav_msg.pose.position.x = _transform.getOrigin().x();
-				nav_msg.pose.position.y = _transform.getOrigin().y();
-				nav_msg.pose.position.z = _transform.getOrigin().z();
-				/*nav_msg.pose.position.x = _transform.getOrigin().z();
-				nav_msg.pose.position.y = _transform.getOrigin().x();
-				nav_msg.pose.position.z = _transform.getOrigin().y(); */
-				
-				/*tf::Matrix3x3 m( _transform.getRotation() ); // quaternion to RPY
-				m.getRPY(roll, pitch, yaw);
-				quaternion.setRPY(0,0,yaw-3.14);  // RPY to quaternion 
-
-
-				nav_msg.pose.orientation.x = quaternion.x();
-				nav_msg.pose.orientation.y = quaternion.y();
-				nav_msg.pose.orientation.z = quaternion.z();
-				nav_msg.pose.orientation.w = quaternion.w();
-			       /*nav_msg.pose.orientation.x = 0;
-				nav_msg.pose.orientation.y = 0;
-				nav_msg.pose.orientation.z = 0;
-				nav_msg.pose.orientation.w = 1;*/
-				
-				ROS_INFO("sending goal");
-				ac.sendGoal(goal);
-				ac.waitForResult();
-				if(ac.getState() == actionlib::SimpleClientGoalState::SUCCEEDED) ROS_INFO("Hooray, the base moved 1 meter forward"); 
-				else  ROS_INFO("The base failed to move forward 1 meter for some reason");
-			}
+			goal.target_pose.header.frame_id="map";
+			goal.target_pose.header.stamp=ros::Time::now();
 			
 
+			goal.target_pose.pose.position.x =  _transform.getOrigin().x();
+			goal.target_pose.pose.position.y =  _transform.getOrigin().y();
 
-			//nav_msg.header.stamp = ros::Time::now();
-			//_goal_pub.publish(nav_msg);
+			tf::Matrix3x3 m( _transform.getRotation() ); // quaternion to RPY
+			m.getRPY(roll, pitch, yaw);
+			quaternion.setRPY(roll,pitch,yaw);  // RPY to quaternion 
+			
+			//DEBUG
+			//quaternion.setRPY(0,0,0.554); 
+			goal.target_pose.pose.orientation.x = quaternion.x() ; 
+			goal.target_pose.pose.orientation.y = quaternion.y() ; 
+			goal.target_pose.pose.orientation.z = quaternion.z() ; 
+			goal.target_pose.pose.orientation.w = quaternion.w() ; 
 
-			_prev_trans.setOrigin(_check_trans.getOrigin());
-			_prev_trans.setRotation(_check_trans.getRotation());
 
+
+			ROS_INFO("sending goal");
+			ac.sendGoal(goal);
+			sent = true;
+			ac.waitForResult();
+			if(ac.getState() == actionlib::SimpleClientGoalState::SUCCEEDED) {
+				ROS_INFO("Hooray, the base moved 1 meter forward");
+			}
+			else {
+				ROS_INFO("The base failed to move forward 1 meter for some reason");
+			}
 		}
         catch (tf::TransformException ex){
             ROS_ERROR("%s",ex.what());
