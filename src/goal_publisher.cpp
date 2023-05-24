9b606d827a0c0684116da02eda7cdea433ebd11e
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 <feesposito00@gmail.com>
2023-05-24 11:02:51 +0200

    Added wander function, code needs some cleaning up, removed ac.waitForResult() as it led to the execution getting stuck

--- src/goal_publisher.cpp
+++ src/goal_publisher.cpp
@@ -7,7 +7,7 @@
 #include "move_base_msgs/MoveBaseAction.h"
 #include <actionlib/client/simple_action_client.h>
 
-#define _freq 1
+#define _freq 0.1
 typedef actionlib::SimpleActionClient<move_base_msgs::MoveBaseAction> MoveBaseClient;
 
 
@@ -19,6 +19,7 @@ class goal_gen {
 		void loop();
 	private:
 		float _disp;
+		float _max_wander;
 		tf::Transform _trans;
 		tf::StampedTransform _check_trans;
 		tf::StampedTransform _prev_trans;
@@ -34,14 +35,16 @@ class goal_gen {
 
 goal_gen::goal_gen(): _rate(_freq) {
 	_disp = 0.3;
+	_max_wander = 0.5;
 	_goal_pub = _nh.advertise<geometry_msgs::PoseStamped>("/move_base_simple/goal", 1);
+	srand(time(NULL));
 	boost::thread(&goal_gen::loop, this);
 }
 
 
 
 void goal_gen::loop() {
-	geometry_msgs::PoseStamped nav_msg;
+	//geometry_msgs::PoseStamped nav_msg;
 	MoveBaseClient ac ("move_base",true);
 	move_base_msgs::MoveBaseGoal goal;
 	bool Tf_equal = false;
@@ -51,55 +54,91 @@ void goal_gen::loop() {
 
 	bool sent = false;
 
-
 	while (!sent && ros::ok()) {
-		try{
-			_listener.waitForTransform("camera_frame", "aruco_marker_frame", ros::Time(0), ros::Duration(60.0));
-			_trans.setOrigin( tf::Vector3(0, 0, _disp) );
-			tf::Quaternion q;
-			q.setRPY(0,1.57,-1.57);
-			_trans.setRotation(q);
-			_trans_br.sendTransform(tf::StampedTransform(_trans, ros::Time::now(), "aruco_marker_frame", "goal_frame"));
-			_listener_goal.waitForTransform("map", "goal_frame", ros::Time(0), ros::Duration(60.0));
-			_listener_goal.lookupTransform("map","goal_frame",ros::Time(0), _transform);
+		bool marker_seen = _listener.waitForTransform("camera_frame", "aruco_marker_frame", ros::Time(0), ros::Duration(1.0));
+		ROS_ERROR("LOOKING FOR MARKER");
+		if (marker_seen) {
+			try{
+				_trans.setOrigin( tf::Vector3(0, 0, _disp) );
+				tf::Quaternion q;
+				q.setRPY(0,1.57,-1.57);
+				_trans.setRotation(q);
+				_trans_br.sendTransform(tf::StampedTransform(_trans, ros::Time::now(), "aruco_marker_frame", "goal_frame"));
+				_listener_goal.waitForTransform("map", "goal_frame", ros::Time(0), ros::Duration(10.0));
+				_listener_goal.lookupTransform("map","goal_frame",ros::Time(0), _transform);
 
-			goal.target_pose.header.frame_id="map";
-			goal.target_pose.header.stamp=ros::Time::now();
-			
+				goal.target_pose.header.frame_id="map";
+				goal.target_pose.header.stamp=ros::Time::now();
+				
 
-			goal.target_pose.pose.position.x =  _transform.getOrigin().x();
-			goal.target_pose.pose.position.y =  _transform.getOrigin().y();
+				goal.target_pose.pose.position.x =  _transform.getOrigin().x();
+				goal.target_pose.pose.position.y =  _transform.getOrigin().y();
 
-			tf::Matrix3x3 m( _transform.getRotation() ); // quaternion to RPY
-			m.getRPY(roll, pitch, yaw);
-			quaternion.setRPY(roll,pitch,yaw);  // RPY to quaternion 
-			
-			//DEBUG
-			//quaternion.setRPY(0,0,0.554); 
-			goal.target_pose.pose.orientation.x = quaternion.x() ; 
-			goal.target_pose.pose.orientation.y = quaternion.y() ; 
-			goal.target_pose.pose.orientation.z = quaternion.z() ; 
-			goal.target_pose.pose.orientation.w = quaternion.w() ; 
+				tf::Matrix3x3 m( _transform.getRotation() ); // quaternion to RPY
+				m.getRPY(roll, pitch, yaw);
+				quaternion.setRPY(roll,pitch,yaw);  // RPY to quaternion 
+				
+				//DEBUG
+				//quaternion.setRPY(0,0,0.554); 
+				goal.target_pose.pose.orientation.x = quaternion.x() ; 
+				goal.target_pose.pose.orientation.y = quaternion.y() ; 
+				goal.target_pose.pose.orientation.z = quaternion.z() ; 
+				goal.target_pose.pose.orientation.w = quaternion.w() ; 
 
-
-
-			ROS_INFO("sending goal");
-			ac.sendGoal(goal);
-			sent = true;
-			ac.waitForResult();
-			if(ac.getState() == actionlib::SimpleClientGoalState::SUCCEEDED) {
-				ROS_INFO("Hooray, the base moved 1 meter forward");
+				ROS_INFO("sending goal");
+				ac.sendGoal(goal);
+				sent = true;
+				/*ac.waitForResult();
+				if(ac.getState() == actionlib::SimpleClientGoalState::SUCCEEDED) {
+					ROS_INFO("Hooray, the base moved 1 meter forward");
+				}
+				else {
+					ROS_INFO("The base failed to move forward 1 meter for some reason");
+				}*/
+				ROS_ERROR("MARKER SEEN"); //DEBUG
 			}
-			else {
-				ROS_INFO("The base failed to move forward 1 meter for some reason");
+			catch (tf::TransformException ex){
+				ROS_ERROR("%s",ex.what());
+				ros::Duration(1.0).sleep();
+			}
+		} else {
+			try{
+				ROS_ERROR("WANDERING TRY");
+				_listener_goal.waitForTransform("map", "base_link", ros::Time(0), ros::Duration(10.0));
+				_listener_goal.lookupTransform("map","base_link",ros::Time(0), _transform);
+				float rand_dist = float(rand())/RAND_MAX*_max_wander;
+				float rand_yaw = float(rand())/RAND_MAX*6.28;
+				ROS_ERROR("%f", rand_dist);
+				ROS_ERROR("%f", rand_yaw);
+
+				goal.target_pose.header.frame_id="map";
+				goal.target_pose.header.stamp=ros::Time::now();
+
+				goal.target_pose.pose.position.x =  _transform.getOrigin().x() + rand_dist*cos(rand_yaw);
+				goal.target_pose.pose.position.y =  _transform.getOrigin().y() + rand_dist*sin(rand_yaw);
+
+				quaternion.setRPY(0,0,rand_yaw);
+
+				goal.target_pose.pose.orientation.x = quaternion.x() ; 
+				goal.target_pose.pose.orientation.y = quaternion.y() ; 
+				goal.target_pose.pose.orientation.z = quaternion.z() ; 
+				goal.target_pose.pose.orientation.w = quaternion.w() ; 
+
+				ac.sendGoal(goal);
+				/*ac.waitForResult();
+				if(ac.getState() == actionlib::SimpleClientGoalState::SUCCEEDED) {
+					ROS_INFO("Hooray, the base moved 1 meter forward");
+				}
+				else {
+					ROS_INFO("The base failed to move forward 1 meter for some reason");
+				}*/
+				ROS_ERROR("WANDERING"); //DEBUG
+			}
+			catch (tf::TransformException ex){
+				ROS_ERROR("%s",ex.what());
+				ros::Duration(1.0).sleep();
 			}
 		}
-        catch (tf::TransformException ex){
-            ROS_ERROR("%s",ex.what());
-            ros::Duration(1.0).sleep();
-        }
-
-
 		_rate.sleep();
 	}
 
